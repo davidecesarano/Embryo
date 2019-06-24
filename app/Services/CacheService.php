c0068a6f0962078dcff36d9426afaf406eb50d13
davidecesarano <davide.cesarano@unipegaso.it>
2019-06-24 15:14:34 +0200

    Fix cache expiration in Cache Service
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -20,7 +20,8 @@
         {
             $this->container->set(Cache::class, function($container){
                 $settings = $container->get('settings');
-                return new Cache($settings['cache']['cachePath']);
+                $cache = new Cache($settings['cache']['cachePath']);
+                return $cache->setDefaultTtl($settings['cache']['ttl']);
             });
 
             $this->container->alias('cache', Cache::class);
