9283bd9352981a33e136a7f9a8d05e6cea918a3d
Davide Cesarano <Dav.Cesarano@unipegaso.local>
2020-07-03 16:10:59 +0200

    Remove vue route

--- routes/app.php
+++ routes/app.php
@@ -5,11 +5,4 @@
         return $view->render($response, 'home', ['title' => 'Embryo 2']);
     });
 
-    $app->get('/vue', function($request, $response){
-
-        $webpack = $this->get('webpack');
-        return $this->get('view')->render($response, 'vue', ['bundle' => $webpack]);
-
-    });
-
     $app->get('/controller/{name}', 'PageController@index');
