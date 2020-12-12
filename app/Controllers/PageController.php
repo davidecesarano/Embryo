ecac0f0c201c48c0f1f91598a6b7010a5e993481
davidecesarano <cesaranodavide@gmail.com>
2020-12-12 16:23:56 +0100

    Add return value

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -7,13 +7,16 @@
     namespace App\Controllers;
     
     use Embryo\Controller;
+    use Psr\Http\Message\ResponseInterface;
 
     class PageController extends Controller
     {   
         /**
          * Home
+         * 
+         * @return ResponseInterface
          */
-        public function index()
+        public function index(): ResponseInterface
         {
             $view = $this->get('view');
             return $view->render($this->response, 'home', ['title' => 'Embryo 2']);
